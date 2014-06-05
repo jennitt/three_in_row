cf4f7b36949f279fe139482e7fde204fda7aece1
jennitt <jane7594@mail.ru>
2014-06-05 22:55:53 +0400

    еще немного поправки

--- Program.cs
+++ Program.cs
@@ -390,7 +390,13 @@ namespace Игра
         {
             ImgSource = Properties.Resources.bomb;
         }
-
+/// <summary>
+/// Активация
+/// </summary>
+/// <param name="posX"></param>
+/// <param name="posY"></param>
+/// <param name="myBoard"></param>
+/// <returns></returns>
         public override bool Activation(int posX, int posY, Board myBoard)
         {
             int lb = posX - 1, rb = posX + 1, tb = posY - 1, bb = posY + 1; // запоминаем координаты границ вокруг ячейки
@@ -417,7 +423,13 @@ namespace Игра
         {
             ImgSource = Properties.Resources.zip;
         }
-
+        /// <summary>
+        /// Активация
+        /// </summary>
+        /// <param name="posX"></param>
+        /// <param name="posY"></param>
+        /// <param name="myBoard"></param>
+        /// <returns></returns>
         public override bool Activation(int posX, int posY, Board myBoard)
         {
             myBoard.Generate(); // генерируем новое игровое поле
